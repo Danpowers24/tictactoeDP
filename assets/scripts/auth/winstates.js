d79776188696893dcb857d9e1dd1ced638d3e5bb
Danpowers24 <danpowers610@gmail.com>
2020-03-14 16:12:14 -0400

    Refactor code that checks for win states
    So I went back through and reworked it so I didn't have to have 2
    separate blocks to check if x and o have won. I wrote it so that it
    would just check if each row was filled with the same player
    regardless of which player it is. I just rewrote it in winstates.js to
    mess around with syntax. The next step will be to copy and paste it
    into events.js to try to call it in the onMove function.

--- assets/scripts/auth/winstates.js
+++ assets/scripts/auth/winstates.js
@@ -3,35 +3,39 @@ const gameState = require('./events.js')
 
 // to avoid circular dependancies, I should work on refactoring some of this code and then copy and paste it back into events.js
 
-// initial export of hard code
-if (gameState[0] === 'x' && gameState[1] === 'x' && gameState[2] === 'x') {
-  console.log('PLAYER X WINS YAYYYYYYYY')
-  $('#message').text('Player X wins!')
-} else if (gameState[3] === 'x' && gameState[4] === 'x' && gameState[5] === 'x') {
-  console.log('PLAYER X WINS YAYYYYYYYY')
-  $('#message').text('Player X wins!')
-} else if (gameState[6] === 'x' && gameState[7] === 'x' && gameState[8] === 'x') {
-  console.log('PLAYER X WINS YAYYYYYYYY')
-  $('#message').text('Player X wins!')
-} else if (gameState[0] === 'x' && gameState[3] === 'x' && gameState[6] === 'x') {
-  console.log('PLAYER X WINS YAYYYYYYYY')
-  $('#message').text('Player X wins!')
-} else if (gameState[1] === 'x' && gameState[4] === 'x' && gameState[7] === 'x') {
-  console.log('PLAYER X WINS YAYYYYYYYY')
-  $('#message').text('Player X wins!')
-} else if (gameState[2] === 'x' && gameState[5] === 'x' && gameState[8] === 'x') {
-  console.log('PLAYER X WINS YAYYYYYYYY')
-  $('#message').text('Player X wins!')
-} else if (gameState[0] === 'x' && gameState[4] === 'x' && gameState[8] === 'x') {
-  console.log('PLAYER X WINS YAYYYYYYYY')
-  $('#message').text('Player X wins!')
-} else if (gameState[2] === 'x' && gameState[4] === 'x' && gameState[6] === 'x') {
-  console.log('PLAYER X WINS YAYYYYYYYY')
-  $('#message').text('Player X wins!')
-} else if (gameState[0] !== '' && gameState[1] !== '' && gameState[2] !== '' && gameState[3] !== '' && gameState[4] !== '' && gameState[5] !== '' && gameState[6] !== '' && gameState[7] !== '' && gameState[8] !== '') {
-  console.log('GAME ENDS IN A TIE')
+// for each console.log (which will be message updates) show winning player by lowest index in winning 3-in-a-row sequence
+
+const checkWin = function () {
+  if (gameState[0] !== '' && gameState[0] === gameState[1] && gameState[1] === gameState[2]) {
+    $('#message').text(`Player ${gameState[0]} WINS`)
+    console.log(`Player ${gameState[0]} WINS`)
+  } else if (gameState[3] !== '' && gameState[3] === gameState[4] && gameState[4] === gameState[5]) {
+    $('#message').text(`Player ${gameState[3]} WINS`)
+    console.log(`Player ${gameState[3]} WINS`)
+  } else if (gameState[6] !== '' && gameState[6] === gameState[7] && gameState[7] === gameState[8]) {
+    $('#message').text(`Player ${gameState[6]} WINS`)
+    console.log(`Player ${gameState[6]} WINS`)
+  } else if (gameState[0] !== '' && gameState[0] === gameState[3] && gameState[3] === gameState[6]) {
+    $('#message').text(`Player ${gameState[0]} WINS`)
+    console.log(`Player ${gameState[0]} WINS`)
+  } else if (gameState[1] !== '' && gameState[1] === gameState[4] && gameState[4] === gameState[7]) {
+    $('#message').text(`Player ${gameState[1]} WINS`)
+    console.log(`Player ${gameState[1]} WINS`)
+  } else if (gameState[2] !== '' && gameState[2] === gameState[5] && gameState[5] === gameState[8]) {
+    $('#message').text(`Player ${gameState[2]} WINS`)
+    console.log(`Player ${gameState[2]} WINS`)
+  } else if (gameState[0] !== '' && gameState[0] === gameState[4] && gameState[4] === gameState[8]) {
+    $('#message').text(`Player ${gameState[0]} WINS`)
+    console.log(`Player ${gameState[0]} WINS`)
+  } else if (gameState[2] !== '' && gameState[2] === gameState[4] && gameState[4] === gameState[6]) {
+    $('#message').text(`Player ${gameState[0]} WINS`)
+    console.log(`Player ${gameState[0]} WINS`)
+  }
 }
 
+  else if (gameState[0] !== '' && gameState[1] !== '' && gameState[2] !== '' && gameState[3] !== '' && gameState[4] !== '' && gameState[5] !== '' && gameState[6] !== '' && gameState[7] !== '' && gameState[8] !== '') {
+    console.log('GAME ENDS IN A TIE')
+  }
 
 
 
